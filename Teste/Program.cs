2f14ff8db82278dd3a480a06104efbc71e24388c
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-16 19:15:34 -0300

    Struct

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -7,23 +7,36 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            int inteiro = 100;
+            // Em struct Pode-se instanciar um objeto sem parâmetro ou com parâmetro
+            var product = new Product(id: 1, name: "Playstation 5", price: 500.00);
 
-            Console.WriteLine(inteiro);
-
-            float real = 25.5f;
-
-            Console.WriteLine(real);
-            //inteiro = real;
-            real = inteiro;
-            Console.WriteLine(real);
-
-            real = 25.5f;
-            inteiro = (int)real;
-            Console.WriteLine(inteiro);
-
-            inteiro = Convert.ToInt32(real);
-            Console.WriteLine(inteiro);
+            var priceInReal = product.PriceInReal(5.45);
+            Console.WriteLine(product);
+            Console.WriteLine($"Price in Real: {priceInReal.ToString("C", CultureInfo.GetCultureInfo("pt-br"))}");
         }
+    }
+    struct Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"id: {Id} - Name: {Name} - Price in dollar: {Price.ToString("C")}";
+        }
+
+        public double PriceInReal(double dollar)
+        {
+            return Price * dollar * 1.65;
+        }
+
+
     }
 }
