f3cd0c77c8b9d7cea36947b49df53e5d4f22c6c1
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-19 19:52:43 -0300

    Guid Example

---
+++ Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guid");
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+            Console.ReadKey();
+        }
+    }
+}
