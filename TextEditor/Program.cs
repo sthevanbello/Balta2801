d6ef99c09a72c9256e2c18dc959963f9fa66bcf4
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-19 17:47:24 -0300

    Text Editor finished

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Threading;
+using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -6,7 +8,90 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose a option");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Create a new file");
+            Console.WriteLine("3 - Exit");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Open();
+                    break;
+                case 2:
+                    Edit();
+                    break;
+                case 3:
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Open()
+        {
+            Console.Clear();
+            Console.Write("Enter the path file: ");
+            string path = Console.ReadLine();
+
+            // using (var file = File.OpenText(path))
+            // {
+            //     while(!file.EndOfStream){
+            //         Console.WriteLine(file.ReadLine());
+            //     }
+            // }
+            using(var file = new StreamReader(path)){
+                Console.WriteLine(file.ReadToEnd());
+            }
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Input a text (ESC to exit)");
+            Console.WriteLine("------------------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+                // text += "\n";
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.Write("Enter the path to save a file: ");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            };
+            Console.WriteLine("Save success");
+            Thread.Sleep(2000);
+            Menu();
         }
     }
+
+
 }
