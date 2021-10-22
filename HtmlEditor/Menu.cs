8b488119007ffccee1d7f9c2895340ee9f6045ac
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-21 21:21:06 -0300

    Created class menu

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -8,9 +8,10 @@ namespace HtmlEditor
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("HTML Editor");
             Console.BackgroundColor = ConsoleColor.Black;
             DrawScreen();
+            WriteOptions();
+            Console.ReadKey();
         }
 
         public static void DrawScreen()
@@ -43,6 +44,27 @@ namespace HtmlEditor
             }
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("HTML Editor");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("=======================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Choose a option");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("-----------------------");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Create a new file");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Open file");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("3 - Exit");
+            Console.SetCursorPosition(3, 9);
+            Console.Write("Option: ");
+
         }
     }
 }
