6201130384a9736a2365fa22349a3df1c3d8136f
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-26 22:08:47 -0300

    String format for DateTime

--- Data/Program.cs
+++ Data/Program.cs
@@ -6,7 +6,19 @@ namespace Data
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            // var data = DateTime.Now;
+
+            var data = new DateTime(2021, 10, 26);
+            
+            Console.WriteLine(data.ToString("dd - MM - yyyy"));
+            Console.WriteLine(data.DayOfWeek);
+
+            var dataHoje = DateTime.Now;
+            var formatada = String.Format("{0:dd-MM-yyyy - ff zzz}", dataHoje);
+            Console.WriteLine(formatada);
+
+            Console.ReadKey();
         }
     }
 }
