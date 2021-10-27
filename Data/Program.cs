3a0726a616358eecead27bda70a2af649597fc7f
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-27 20:48:54 -0300

    DateTime tests

--- Data/Program.cs
+++ Data/Program.cs
@@ -17,7 +17,14 @@ namespace Data
             var dataHoje = DateTime.Now;
             var formatada = String.Format("{0:dd-MM-yyyy - ff zzz}", dataHoje);
             Console.WriteLine(formatada);
-
+            var formatada2 = String.Format("{0:G}", dataHoje);
+            var formatada3 = String.Format("{0:r}", dataHoje);
+            var formatada4 = String.Format("{0:s}", dataHoje);
+            var formatada5 = String.Format("{0:u}", dataHoje);
+            Console.WriteLine(formatada2);
+            Console.WriteLine(formatada3);
+            Console.WriteLine(formatada4);
+            Console.WriteLine(formatada5);
             Console.ReadKey();
         }
     }
