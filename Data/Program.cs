f7bd8db8022472034a86ed7a1314e723f9dcd552
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-01 14:28:26 -0300

    TimeSpan

--- Data/Program.cs
+++ Data/Program.cs
@@ -17,60 +17,65 @@ namespace Data
             Console.WriteLine(data.DayOfWeek);
 
             var dataHoje = DateTime.Now;
-            var formatada = String.Format("{0:dd-MM-yyyy - ff zzz}", dataHoje);
-            Console.WriteLine(formatada);
-            var formatada2 = String.Format("{0:G}", dataHoje);
-            var formatada3 = String.Format("{0:r}", dataHoje);
-            var formatada4 = String.Format("{0:s}", dataHoje);
-            var formatada5 = String.Format("{0:u}", dataHoje);
-            Console.WriteLine(formatada2);
-            Console.WriteLine(formatada3);
-            Console.WriteLine(formatada4);
-            Console.WriteLine(formatada5);
+            // var formatada = String.Format("{0:dd-MM-yyyy - ff zzz}", dataHoje);
+            // Console.WriteLine(formatada);
+            // var formatada2 = String.Format("{0:G}", dataHoje);
+            // var formatada3 = String.Format("{0:r}", dataHoje);
+            // var formatada4 = String.Format("{0:s}", dataHoje);
+            // var formatada5 = String.Format("{0:u}", dataHoje);
+            // Console.WriteLine(formatada2);
+            // Console.WriteLine(formatada3);
+            // Console.WriteLine(formatada4);
+            // Console.WriteLine(formatada5);
 
-            var dataAdd = DateTime.Now;
-            var newData = dataAdd.AddDays(30);
-            Console.WriteLine(newData);
+            // var dataAdd = DateTime.Now;
+            // var newData = dataAdd.AddDays(30);
+            // Console.WriteLine(newData);
 
-            dataAdd = DateTime.Now;
-            newData = dataAdd.AddMonths(6);
-            Console.WriteLine(newData);
+            // dataAdd = DateTime.Now;
+            // newData = dataAdd.AddMonths(6);
+            // Console.WriteLine(newData);
 
-            dataAdd = DateTime.Now;
-            newData = dataAdd.AddYears(6);
-            Console.WriteLine(newData);
+            // dataAdd = DateTime.Now;
+            // newData = dataAdd.AddYears(6);
+            // Console.WriteLine(newData);
 
-            newData = dataAdd.AddYears(-2);
-            Console.WriteLine(newData);
+            // newData = dataAdd.AddYears(-2);
+            // Console.WriteLine(newData);
 
-            Console.WriteLine("\n================================\n");
+            // Console.WriteLine("\n================================\n");
 
-            var ptBr = new CultureInfo("pt-BR");
-            var ptPt = new CultureInfo("pt-PT");
-            var en = new CultureInfo("en-US");
-            var de = new CultureInfo("de-DE");
+            // var ptBr = new CultureInfo("pt-BR");
+            // var ptPt = new CultureInfo("pt-PT");
+            // var en = new CultureInfo("en-US");
+            // var de = new CultureInfo("de-DE");
 
-            Console.WriteLine(DateTime.Now.ToString("D", ptBr));
-            Console.WriteLine(DateTime.Now.ToString("D", ptPt));
-            Console.WriteLine(DateTime.Now.ToString("D", en));
-            Console.WriteLine(DateTime.Now.ToString("D", de));
+            // Console.WriteLine(DateTime.Now.ToString("D", ptBr));
+            // Console.WriteLine(DateTime.Now.ToString("D", ptPt));
+            // Console.WriteLine(DateTime.Now.ToString("D", en));
+            // Console.WriteLine(DateTime.Now.ToString("D", de));
 
-            Console.WriteLine("\n================================\n");
-            var dataUtc = DateTime.UtcNow;
+            // Console.WriteLine("\n================================\n");
+            // var dataUtc = DateTime.UtcNow;
 
-            Console.WriteLine(dataUtc);
+            // Console.WriteLine(dataUtc);
 
-            Console.WriteLine(dataUtc.ToLocalTime());
+            // Console.WriteLine(dataUtc.ToLocalTime());
 
-            Console.WriteLine("\n================================\n");
-            var timezones = TimeZoneInfo.GetSystemTimeZones();
-            foreach (var timezone in timezones)
-            {
-                Console.WriteLine(timezone.Id);
-                Console.WriteLine(timezone);
-                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(dataUtc, timezone));
-                Console.WriteLine("_________");
-            }
+            // Console.WriteLine("\n================================\n");
+            // var timezones = TimeZoneInfo.GetSystemTimeZones();
+            // foreach (var timezone in timezones)
+            // {
+            //     Console.WriteLine(timezone.Id);
+            //     Console.WriteLine(timezone);
+            //     Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(dataUtc, timezone));
+            //     Console.WriteLine("_________");
+            // }
+
+            var timeSpan = new TimeSpan(1);
+            Console.WriteLine(timeSpan);
+            var timeSpan2 = new TimeSpan(1, 15, 25);
+            Console.WriteLine(timeSpan2);
 
             Console.ReadKey();
         }
