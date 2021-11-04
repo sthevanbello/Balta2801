60c0277dd43b753b8a0f82fc61bcc6e95b9b8c9f
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-04 10:53:53 -0300

    Working with array

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,7 +6,19 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Working with Array");
+            var meuArray = new int[2]{2, 6};
+
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
         }
     }
 }
