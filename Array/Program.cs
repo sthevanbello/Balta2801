33fd4922feae1b71e4591b18f4a133d3e8419f18
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-04 11:36:54 -0300

    Try Catch Exception

--- Array/Program.cs
+++ Array/Program.cs
@@ -8,7 +8,7 @@ namespace Array
         {
             Console.Clear();
             Console.WriteLine("Working with Array");
-            var meuArray = new int[2]{2, 6};
+            var meuArray = new int[2] { 2, 6 };
 
             Console.WriteLine(meuArray[0]);
             Console.WriteLine(meuArray[1]);
@@ -17,8 +17,18 @@ namespace Array
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("\n==== Try Catch (Exception) ====\n");
+            try
+            {
+                Console.WriteLine(meuArray[0]);
+                Console.WriteLine(meuArray[1]);
+                Console.WriteLine(meuArray[2]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
-
         }
     }
 }
