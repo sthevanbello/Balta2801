6a5b1a4581e2f2c42c37e2f28fc039cc7af1979d
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-03 23:45:48 -0300

    Finished Money

--- Money/Program.cs
+++ Money/Program.cs
@@ -10,8 +10,18 @@ namespace Money
             Console.WriteLine("Working with Money");
             Console.WriteLine("==================================\n");
 
-            var valor = 10.25m;
+            var valor = 10.57m;
+            decimal valorDecimal = 10.25m;
+
             Console.WriteLine(valor.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+            Console.WriteLine(valorDecimal.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+            Console.WriteLine(valorDecimal.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valorDecimal.ToString("C", CultureInfo.CreateSpecificCulture("de-DE")));
+
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Abs(valor));
 
             Console.ReadKey();
         }
